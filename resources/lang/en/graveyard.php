c1d389a96c8edd09e63f3a4cfe1523bb2da915f5
ScroogeHD117 <charlie@dehaas.ch>
2018-04-20 16:55:52 +0200

    Fix grammatical errors on graveyard page

--- resources/lang/en/graveyard.php
+++ resources/lang/en/graveyard.php
@@ -7,11 +7,11 @@
 return [
     'graveyard' => 'Graveyard',
     'guidelines' => 'Guidelines',
-    'guidelines-content' => '1) You cannot resurrect your own uploads.<br>2) Dont resurrect something you cannot commit too.',
+    'guidelines-content' => '1) You cannot resurrect your own uploads.<br>2) Do not resurrect a torrent that you cannot commit too.',
     'dead' => 'Dead',
     'ressurect' => 'Ressurect',
     'pending' => 'Pending',
-    'howto' => 'Heres The Rule',
+    'howto' => 'Here Is The Rule',
     'howto-desc1' => 'You must seed <span class="text-green"><em>:name</em></span> for <span class="text-red text-bold">30 days</span> for a succesful ressurection. In which case when your current seedtime of',
     'howto-hits' => 'Hits',
     'howto-desc2' => 'You will be rewarded',

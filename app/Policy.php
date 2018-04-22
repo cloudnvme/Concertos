dcebada974b9d627a46ba26b938cc5c7908060d0
Hyleus <Hyleus@tuta.io>
2018-04-22 13:26:43 +0200

    Fix method modifiers

--- app/Policy.php
+++ app/Policy.php
@@ -7,17 +7,17 @@ use Carbon\Carbon;
 
 class Policy
 {
-    static function isModerator(User $user)
+    public static function isModerator(User $user)
     {
         return $user->group->is_modo;
     }
 
-    static function canEditTorrent(User $user, Torrent $torrent)
+    public static function canEditTorrent(User $user, Torrent $torrent)
     {
         return self::isModerator($user) || $torrent->user->id == $user->id;
     }
 
-    static function canDeleteTorrent(User $user, Torrent $torrent)
+    public static function canDeleteTorrent(User $user, Torrent $torrent)
     {
         return self::isModerator($user) || ($user->id == $torrent->user_id && Carbon::now()->lt($torrent->created_at->addDay()));
     }

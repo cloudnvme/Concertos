80a21eb274b6f2a0f01643f5385d42f53465f51b
Hyleus <Hyleus@tuta.io>
2018-04-23 18:30:11 +0200

    Fix activation controller

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -28,7 +28,7 @@ class ActivationController extends Controller
             $activation->user->active = true;
             $activation->user->removeRole('Validating');
             $activation->user->addRole('User');
-            $actvation->user->setMainRole('User');
+            $activation->user->setMainRole('User');
             $activation->user->save();
 
             // Activity Log

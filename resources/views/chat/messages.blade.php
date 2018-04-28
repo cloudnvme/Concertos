5864982ca33d39a56605b0dddb705ea22f0a9296
Hyleus <Hyleus@tuta.io>
2018-04-28 10:58:34 +0200

    Fix chatbox messages

--- resources/views/chat/messages.blade.php
+++ resources/views/chat/messages.blade.php
@@ -1,7 +1,7 @@
 @foreach($messages as $message)
-  <div class="chat-message">
-    <div class="chat-message__info">
-      <div style="color: {{ $message->poster->roleColor() }}" class="chat-message__poster">
+  <div class="message">
+    <div class="message__info">
+      <div style="color: {{ $message->poster->roleColor() }}" class="message__user">
         <a class="link" href="{{ route('profile', ['id' => $message->poster->id]) }}">
           <i class="{{ $message->poster->roleIcon() }}"></i>
           {{ $message->poster->roleName() }}
@@ -9,12 +9,12 @@
         </a>
       </div>
       @if ($message->poster->image != null)
-        <img class="chat-message__avatar" src="{{ url("files/img/{$message->poster->image}") }}"></img>
+        <img class="message__avatar" src="{{ url("files/img/{$message->poster->image}") }}"></img>
       @else
-        <img class="chat-message__avatar" src="{{ url("img/profile.png") }}"></img>
+        <img class="message__avatar" src="{{ url("img/profile.png") }}"></img>
       @endif
-      <div class="chat-message__time">{{ $message->created_at }}</div>
+      <div class="message__time">{{ $message->created_at }}</div>
     </div>
-    <div class="chat-message__text">{!! $message->asHtml() !!}</div>
+    <div class="message__text">{!! $message->asHtml() !!}</div>
   </div>
 @endforeach

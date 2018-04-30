90cf1731d8b13dae9fd12a065553444353d183e9
Hyleus <Hyleus@tuta.io>
2018-04-30 13:41:15 +0200

    Rework PM system

--- resources/views/pm/outbox.blade.php
+++ resources/views/pm/outbox.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layout.next')
 
 @section('breadcrumb')
 <li class="active">
@@ -9,47 +9,79 @@
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="header gradient silver">
-    <div class="inner_content">
-      <h1>{{ trans('pm.private') }} {{ trans('pm.messages') }} - {{ trans('pm.outbox') }}</h1>
+  <div class="mbox mbox--small-bottom">
+    <a href="{{ route('create', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fa fa-pencil-alt"></i>
+        {{ trans('pm.new') }}
+      </button>
+    </a>
+    <a href="{{ route('inbox', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fas fa-cloud-download-alt"></i>
+        {{ trans('pm.inbox') }}
+      </button>
+    </a>
+    <a href="{{ route('outbox', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fas fa-cloud-upload-alt"></i>
+        {{ trans('pm.outbox') }}
+      </button>
+    </a>
+  </div>
+
+  <div class="block">
+    <div class="block__title">Messages</div>
+    <div class="block__content">
+      <table class="table">
+        <thead>
+        <tr>
+          <th>
+            <label class="v-checkbox">
+              <input id="check" type="checkbox" name="pm_id">
+              <span></span>
+            </label>
+          </th>
+          <th>To</th>
+          <th>{{ trans('pm.subject') }}</th>
+          <th>Sent at</th>
+          <th>{{ trans('pm.read') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($pms as $p)
+          {{ Form::hidden('invisible', 'id', array('id' => 'id')) }}
+          <tr>
+            <td>
+              <label class="v-checkbox">
+                <input id="check" type="checkbox" name="pm_id">
+                <span></span>
+              </label>
+            </td>
+            <td>
+              <a class="link" href="{{ route('profile', ['id' => $p->receiver->id]) }}" title="">{{ $p->receiver->username}}</a>
+            </td>
+            <td>
+              <a class="link" href="{{ route('message', ['id' => $user->id , 'pmid' => $p->id]) }}">{{ $p->subject }}</a>
+            </td>
+            <td>
+              {{ $p->created_at->diffForHumans() }}
+            </td>
+
+            @if ($p->read == 0)
+              <td>
+                <span class='label label--danger'>{{ trans('pm.unread') }}</span>
+              </td>
+            @else ($p->read >= 1)
+              <td>
+                <span class='label label--success'>{{ trans('pm.read') }}</span>
+              </td>
+            @endif
+          </tr>
+          {{ Form::close() }}
+        @endforeach
+        </tbody>
+      </table>
     </div>
   </div>
-        <div class="row">
-          <div class="col-md-2">
-            <div class="block">
-              <a href="{{ route('create', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.new') }}</a>
-              <div class="separator"></div>
-              <div class="list-group">
-                <a href="{{ route('inbox', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.inbox') }}</a>
-                <a href="{{ route('outbox', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.outbox') }}</a>
-              </div>
-            </div>
-          </div>
-
-        <div class="col-md-10">
-            <div class="block">
-          <table class="table table-striped table-hover table-bordered">
-            <thead>
-              <tr>
-                <td class="col-sm-2">{{ trans('pm.to') }}</td>
-                <td class="col-sm-6">{{ trans('pm.subject') }}</td>
-                <td class="col-sm-2">{{ trans('pm.sent-at') }}</td>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach($pms as $p)
-              <tr>
-                <td class="col-sm-2"><a href="{{ route('profile', ['id' => $p->receiver->id]) }}" title="">{{ $p->receiver->username}}</a></td>
-                <td class="col-sm-5"><a href="{{ route('message', ['id' => $user->id , 'pmid' => $p->id]) }}">{{ $p->subject }}</a></td>
-                <td class="col-sm-2">{{ $p->created_at->diffForHumans() }}</td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-          {{ $pms->links() }}
-        </div>
-      </div>
-    </div>
-    </div>
-  @endsection
+@endsection
--- resources/views/pm/send.blade.php
+++ resources/views/pm/send.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layout.next')
 
 @section('stylesheets')
 <link rel="stylesheet" href="{{ url('files/wysibb/theme/default/wbbtheme.css') }}">
@@ -13,66 +13,49 @@
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="header gradient silver">
-    <div class="inner_content">
-      <h1>{{ trans('pm.private') }} {{ trans('pm.message') }} - {{ trans('pm.create') }}</h1>
+  <div class="mbox mbox--small-bottom">
+    <a href="{{ route('create', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fa fa-pencil-alt"></i>
+        {{ trans('pm.new') }}
+      </button>
+    </a>
+    <a href="{{ route('inbox', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fas fa-cloud-download-alt"></i>
+        {{ trans('pm.inbox') }}
+      </button>
+    </a>
+    <a href="{{ route('outbox', array('id' => auth()->user()->id)) }}">
+      <button class="btn">
+        <i class="fas fa-cloud-upload-alt"></i>
+        {{ trans('pm.outbox') }}
+      </button>
+    </a>
+  </div>
+
+  <div class="block">
+    <div class="block__title">Compose</div>
+    <div class="block__content">
+      <form role="form" method="POST" action="{{ route('send-pm') }}">
+        {{ csrf_field() }}
+        <div class="flex mbox mbox--mini-bottom">
+          <div class="badge col col--small mbox mbox--mini-right">To</div>
+          <input name="receiver" class="flex__expanded" type="text" required/>
+        </div>
+
+        <div class="flex mbox mbox--small-bottom">
+          <div class="badge col col--small mbox mbox--mini-right">Subject</div>
+          <input name="subject" class="flex__expanded" type="text" required/>
+        </div>
+
+        <textarea id="message" name="message" class="textarea textarea--vertical" rows=20></textarea>
+
+        <button class="btn col col--medium">
+          <i class="far fa-paper-plane"></i>
+          Send
+        </button>
+      </form>
     </div>
   </div>
-        <div class="row">
-          <div class="col-md-2">
-            <div class="block">
-              <a href="{{ route('create', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.new') }}</a>
-              <div class="separator"></div>
-              <div class="list-group">
-                <a href="{{ route('inbox', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.inbox') }}</a>
-                <a href="{{ route('outbox', array('id' => auth()->user()->id)) }}" class="btn btn-primary btn-block">{{ trans('pm.outbox') }}</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-10">
-            <div class="block">
-                  <form role="form" method="POST" action="{{ route('send-pm') }}">
-                  {{ csrf_field() }}
-
-                  <div class="form-group">
-                    <label for="receiver">User</label>
-                    <input name="receiver" class="form-control" placeholder="User" required>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="">{{ trans('pm.subject') }}</label>
-                    <input name="subject" class="form-control" placeholder="{{ trans('pm.enter-subject') }}" required>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="">{{ trans('pm.message') }}</label>
-                    <textarea id="message" name="message" cols="30" rows="10" class="form-control"></textarea>
-                  </div>
-
-                  <button class="btn btn-primary">
-                    <i class="fa fa-save"></i> {{ trans('pm.send') }}
-                  </button>
-              </form>
-      </div>
-    </div>
-  </div>
-</div>
-@endsection
-
-@section('javascripts')
-    <script type="text/javascript">
-    $(document).ready(function() {
-        $('.js-example-basic-single').select2();
-    });
-    </script>
-
-    <script type="text/javascript" src="{{ url('files/wysibb/jquery.wysibb.js') }}"></script>
-    <script>
-    $(document).ready(function() {
-        var wbbOpt = { }
-        $("#message").wysibb(wbbOpt);
-    });
-    </script>
 @endsection

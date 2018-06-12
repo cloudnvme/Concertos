71632e2958251fe5ecfd6749cba2e8966e1b3633
Hyleus <Hyleus@tuta.io>
2018-06-12 09:30:46 +0200

    Restyle edit-profile page

--- resources/views/user/edit_profile.blade.php
+++ resources/views/user/edit_profile.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layout.next')
 
 @section('breadcrumb')
 <li>
@@ -14,39 +14,36 @@
 @endsection
 
 @section('content')
-<div class="container">
-<div class="block block-form">
-        <h1>{{ trans('user.edit-profile') }}</h1>
+    <div class="block">
+        <div class="block__title">{{ trans('user.edit-profile') }}</div>
+        <div class="block__content">
+            <form action="{{ route('user_edit_profile', ['id' => $user->id]) }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mbox mbox--small-bottom flex">
+                    <span class="badge badge--centered col col--medium mbox--small-right">{{ trans('user.avatar') }}</span>
+                    <input type="file" name="image">
+                </div>
 
-        {{ Form::open(array('route' => array('user_edit_profile', 'id' => $user->id), 'files' => true)) }}
-        {{ csrf_field() }}
-            <div class="form-group">
-                <label for="image">{{ trans('user.avatar') }}</label><small>{{ trans('user.formats-are-supported', ['formats' => '.jpg , .jpeg , .bmp , .png , .tiff , .gif']) }}</small>
-                <input type="file" name="image">
-            </div>
+                <div class="flex mbox mbox--small-bottom">
+                    <div class="badge badge--centered mbox mbox--mini-right col col--medium">{{ trans('user.custom-title') }}</div>
+                    <input type="text" name="title" class="flex__expanded" value="{{ $user->title }}">
+                </div>
 
-            <div class="form-group">
-                <label for="title">{{ trans('user.custom-title') }}</label>
-                <input type="text" name="title" class="form-control" value="{{ $user->title }}">
-            </div>
+                <div class="mbox mbox--small-bottom">
+                    <div class="badge badge--centered mbox--mini-bottom col col--medium">{{ trans('user.about-me') }}</div>
+                    <textarea name="about" class="textarea textarea--vertical">{{ $user->about }}</textarea>
+                </div>
 
-            <div class="form-group">
-                <label for="about">{{ trans('user.about-me') }} <span class="badge-extra">BBCode {{ trans('common.is-allowed') }}</span></label>
-                <textarea name="about" cols="30" rows="10" maxlength="500" class="form-control">{{ $user->about }}</textarea>
-            </div>
+                <div class="mbox mbox--small-botttom">
+                    <div class="badge badge--centered mbox mbox--mini-bottom col col--medium">{{ trans('user.forum-signature') }}</div>
+                    <textarea name="signature" class="textarea textarea--vertical">{{ $user->signature }}</textarea>
+                </div>
 
-            <div class="form-group">
-                <label for="signature">{{ trans('user.forum-signature') }} <span class="badge-extra">BBCode {{ trans('common.is-allowed') }}</span></label>
-                <textarea name="signature" id="" cols="30" rows="10" class="form-control">{{ $user->signature }}</textarea>
-            </div>
-
-            @if( !is_null($user->signature))
-            <center><p>{{ trans('user.forum-signature') }} </p> {!! $user->getSignature() !!}</center>
-            @endif
-
-            <button type="submit" class="btn btn-primary">{{ trans('common.submit') }}</button>
-        {{ Form::close() }}
-
+                <button type="submit" class="btn">
+                    <i class="far fa-paper-plane"></i>
+                    {{ trans('common.submit') }}
+                </button>
+            </form>
+        </div>
     </div>
-</div>
 @endsection

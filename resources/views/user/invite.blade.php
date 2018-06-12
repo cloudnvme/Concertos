2e65eed7fc923877ed752a4978fed9aa4278c238
Hyleus <Hyleus@tuta.io>
2018-06-12 09:01:21 +0200

    Restyle invite page

--- resources/views/user/invite.blade.php
+++ resources/views/user/invite.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layout.next')
 
 @section('title')
     <title>{{ trans('user.invites') }} - {{ config('other.title') }}</title>
@@ -13,21 +13,26 @@
 @endsection
 
 @section('content')
-  <div class="container">
-    <div class="block compose-invite">
-      <h1>Invite a User</h1>
+  <div class="block">
+    <div class="block__title">Invite a User</div>
+    <div class="block__content">
       <p>{{ trans('user.invites-count', ['count' => $user->invites]) }}</p>
       <form action="{{ route('invite') }}" method="post">
         {{ csrf_field() }}
-        <div class="form-group">
-          <label for="email" class="col-sm-2 label label-default">{{ trans('common.email') }}</label>
-          <input class="bar" name="email" type="email" id="email" size="10" required>
+        <div class="flex mbox mbox--small-bottom">
+          <span for="email" class="badge col col--small badge--centered mbox mbox--mini-right">
+            <i class="far fa-envelope mbox mbox--mini-right"></i>
+            {{ trans('common.email') }}
+          </span>
+          <input class="flex__expanded" name="email" type="email" id="email" size="10" required>
         </div>
 
         <h3>{{ trans('common.message') }}</h3>
-        <textarea class="bar" name="message" cols="50" rows="10" id="message"></textarea>
-
-        <button type="submit" class="v-button">{{ trans('common.submit') }}</button>
+        <textarea class="textarea textarea--vertical" name="message" cols="50" rows="10" id="message"></textarea>
+        <button type="submit" class="btn">
+          <i class="far fa-paper-plane"></i>
+          {{ trans('common.submit') }}
+        </button>
       </form>
     </div>
   </div>

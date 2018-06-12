df5d9aea72f074fed43c12929a996495d52795b8
Hyleus <Hyleus@tuta.io>
2018-06-12 09:37:08 +0200

    Fix user title

--- resources/views/user/title.blade.php
+++ resources/views/user/title.blade.php
@@ -1,7 +1,7 @@
-<div class="text-bold" @if ($colored) style="color: {{ $user->roleColor() }}" @endif>
+<span class="text-bold" @if ($colored) style="color: {{ $user->roleColor() }}" @endif>
   <a class="link" href="{{ route('profile', ['id' => $user->id]) }}">
     </span><i class="{{ $user->roleIcon() }}"></i>
     {{ $user->roleName() }}
     {{ $user->username }}
   </a>
-</div>
+</span>

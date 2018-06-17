69fcc2f00c42a43555688963f3c245bf5cf2e69c
Hyleus <Hyleus@tuta.io>
2018-06-17 11:05:03 +0200

    Restyle invite-tree

--- resources/views/user/invitetree.blade.php
+++ resources/views/user/invitetree.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.default')
+@extends('layout.next')
 
 @section('title')
     <title>{{ trans('user.invite-tree') }} - {{ config('other.title') }}</title>
@@ -13,69 +13,56 @@
 @endsection
 
 @section('content')
-<div class="container">
-<div class="block">
-  <h2><a class="view-user" data-id="{{ $user->id }}" data-slug="{{ $user->username }}" href="{{ route('profile', ['id' => $user->id]) }}">{{ $user->username }}</a>
-{{ trans('user.invite-tree') }}</h2>
-  <hr>
-  <div class="row">
-  <div class="col-sm-12">
-  <h2>{{ trans('user.invites-sent') }}</h2>
-  <table class="table table-condensed table-striped table-bordered table-hover">
+  <table class="table table--bordered">
     <thead>
-      <tr>
-        <th>{{ trans('user.sender') }}</th>
-        <th>{{ trans('common.email') }}</th>
-        <th>{{ trans('user.code') }}</th>
-        <th>{{ trans('user.created-on') }}</th>
-        <th>{{ trans('user.expires-on') }}</th>
-        <th>{{ trans('user.accepted-by') }}</th>
-        <th>{{ trans('user.accepted-at') }}</th>
-      </tr>
+    <tr>
+      <th>{{ trans('user.sender') }}</th>
+      <th>{{ trans('common.email') }}</th>
+      <th>{{ trans('user.code') }}</th>
+      <th>{{ trans('user.created-on') }}</th>
+      <th>{{ trans('user.expires-on') }}</th>
+      <th>{{ trans('user.accepted-by') }}</th>
+      <th>{{ trans('user.accepted-at') }}</th>
+    </tr>
     </thead>
     <tbody>
-      @if(count($records) == 0)
+    @if(count($records) == 0)
       <p>{{ trans('user.no-logs') }}</p>
-      @else
-    @foreach($records as $record)
-      <tr>
-        <td>
-			     <a class="view-user" data-id="{{ $record->sender->id }}" data-slug="{{ $record->sender->username }}" href="{{ route('profile', ['username' =>  $record->sender->username, 'id' => $record->sender->id]) }}">{{ $record->sender->username }}</a>
-        </td>
-        <td>
-          {{ $record->email }}
-        </td>
-        <td>
-          {{ $record->code }}
-        </td>
-        <td>
-          {{ $record->created_at }}
-        </td>
-        <td>
-          {{ $record->expires_on }}
-        </td>
-        <td>
-          @if($record->accepted_by != null)
-          <a class="view-user" data-id="{{ $record->reciever->id }}" data-slug="{{ $record->reciever->username }}" href="{{ route('profile', ['username' =>  $record->reciever->username, 'id' => $record->reciever->id]) }}">{{ $record->reciever->username }}</a>
-          @else
-          N/A
-          @endif
-        </td>
-        <td>
-          @if($record->accepted_at != null)
-          {{ $record->accepted_at }}
-          @else
-          N/A
-          @endif
-        </td>
-      </tr>
+    @else
+      @foreach($records as $record)
+        <tr>
+          <td>
+            <a class="view-user" data-id="{{ $record->sender->id }}" data-slug="{{ $record->sender->username }}" href="{{ route('profile', ['username' =>  $record->sender->username, 'id' => $record->sender->id]) }}">{{ $record->sender->username }}</a>
+          </td>
+          <td>
+            {{ $record->email }}
+          </td>
+          <td>
+            {{ $record->code }}
+          </td>
+          <td>
+            {{ $record->created_at }}
+          </td>
+          <td>
+            {{ $record->expires_on }}
+          </td>
+          <td>
+            @if($record->accepted_by != null)
+              <a class="view-user" data-id="{{ $record->reciever->id }}" data-slug="{{ $record->reciever->username }}" href="{{ route('profile', ['username' =>  $record->reciever->username, 'id' => $record->reciever->id]) }}">{{ $record->reciever->username }}</a>
+            @else
+              N/A
+            @endif
+          </td>
+          <td>
+            @if($record->accepted_at != null)
+              {{ $record->accepted_at }}
+            @else
+              N/A
+            @endif
+          </td>
+        </tr>
       @endforeach
-      @endif
+    @endif
     </tbody>
   </table>
-  </div>
-</div>
-
-</div>
-</div>
 @endsection

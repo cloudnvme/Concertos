fc737eadb58e44fd05460e43fb0885e7b9d61af3
Hyleus <Hyleus@tuta.io>
2018-06-17 14:31:32 +0200

    Fix invalid HTML structure in titles

--- resources/views/user/title.blade.php
+++ resources/views/user/title.blade.php
@@ -1,6 +1,6 @@
 <span class="text-bold" @if ($colored) style="color: {{ $user->roleColor() }}" @endif>
   <a class="link" href="{{ route('profile', ['id' => $user->id]) }}">
-    </span><i class="{{ $user->roleIcon() }}"></i>
+    <i class="{{ $user->roleIcon() }}"></i>
     {{ $user->roleName() }}
     {{ $user->username }}
   </a>
